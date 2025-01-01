d26c80d3d573fa3091ce75b066ceb3fdc29971f9
KedarKandel <kandelkedar07@gmail.com>
2025-01-01 23:59:19 +0200

    solve reverse int

--- reverseString.js
+++ reverseString.js
@@ -7,4 +7,19 @@ function reverse(str) {
   }
   return reversed;
 }
-console.log(reverse("Hi"));
+// console.log(reverse("Hi"));
+
+// Given an integer, return an integer that is the reverse ordering of integer.
+
+function reverseInt(num) {
+  if (num === 0) return 0;
+
+  let reversed = "";
+
+  for (let n of String(Math.abs(num))) {
+    reversed = n + reversed;
+  }
+  return num < 0 ? -reversed : reversed;
+}
+
+console.log(reverseInt(-891));
